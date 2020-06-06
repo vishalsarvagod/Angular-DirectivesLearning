b4883c308cb30157ec6fced1baac48b2b190bfab
Vishal Sarvagod <vishalsarvagod91@gmail.com>
2020-06-06 18:08:03 -0400

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'DirectivesLearning';
+  numbers = [1,2,3,4,5,6,7,8,9];
+  oddNumbers =[1,3,5,7,9];
+  evenNumbers =[2,4,6,8];
+  onlyOdd = false;
 }
